4b60d00eca9824f39c39b8d674b43ee431747815
LoyleZhou <123176205@qq.com>
2018-02-03 21:47:09 +0800

    Site updated: 2018-02-03 21:47:08

--- lailai/function.js
+++ lailai/function.js
@@ -6,3 +6,12 @@ function lai(a,b){
 lai('你好啊','来吧哥哥');
 lai('来吧哥哥');
 lai('你好吗','')
+
+// ES6 允许为函数的参数设置默认值，即直接写在参数定义的后面
+
+function lai1(a, b = '来吧哥哥'){
+    console.log(a, b);
+}
+lai1('hello', 'Loyle');
+lai1('hello');
+lai1('hello', '');
